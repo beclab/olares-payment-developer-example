14820bd08972543eb310de71c9f3ee08f913f575
Tang Wei <tw7613781@gmail.com>
2026-09-15 13:32:44 +0800

    fix(config): revert API keys to placeholders for security

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 export const CONFIG = {
   port: 32000,
   shopPublicUrl: 'http://127.0.0.1:32000',
-  apiKey: 'pk_live_7728ecbca5a8414f8210ac71',
-  apiSecret: 'sk_live_d2be6a55d6e14f1aa4aacbc2a67c5d13',
+  apiKey: 'pk_live_REPLACE_ME',
+  apiSecret: 'sk_live_REPLACE_ME',
   webhookSecret: 'whsec_REPLACE_ME',
 };
